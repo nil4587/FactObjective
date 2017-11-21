b0928fa00c61a20f3e34cad2381aa467cecc7294
Nilesh Prajapati <nilesh.prajapati@gatewaytechnolabs.com>
2017-11-21 20:09:36 +0530

    - New Add-ons has been added for NSURLConnection.

--- FactObjective/AppViewControllers/FactsListViewController.m
+++ FactObjective/AppViewControllers/FactsListViewController.m
@@ -15,12 +15,18 @@
 //-- Private declaration properties
 @interface FactsListViewController ()
 @property(strong, nonatomic) UIRefreshControl *refreshControler;
+@property(strong, nonatomic) NSURLConnection *connection;
 @property(strong, nonatomic) NSArray *arrFacts;
+@property(strong, nonatomic) NSMutableData *data;
+@property(strong, nonatomic) NSURLResponse *response;
 @end
 
 @implementation FactsListViewController
 @synthesize refreshControler = _refreshControler;
 @synthesize arrFacts = _arrFacts;
+@synthesize connection = _connection;
+@synthesize data = _data;
+@synthesize response = _response;
 
 #pragma mark - ==================================
 #pragma mark View Life-cycle
@@ -57,6 +63,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) dealloc {
+    //-- To reset the web-service related values
+    [self resetProperties];
+    if (_arrFacts) {
+        _arrFacts = nil;
+    }
+    _refreshControler = nil;
+}
+
+#pragma mark - ==================================
+#pragma mark User-defined methods
+#pragma mark ==================================
+
+- (void)resetProperties {
+    //-- Remove the old data prior making a web-service call
+    _data = nil;
+    //-- Remove the old response prior making a web-service call
+    _response = nil;
+    //-- Cancel the previous operation prior starting a new operation
+    if (_connection) {
+        [_connection cancel];
+        _connection = nil;
+    }
+}
+
+- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+    //UIGraphicsBeginImageContext(newSize);
+    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+    // Pass 1.0 to force exact pixel size.
+    
+    CGSize size = image.size;
+    
+    CGFloat widthRatio  = newSize.width  / image.size.width;
+    CGFloat heightRatio = newSize.height / image.size.height;
+    
+    // Figure out what our orientation is, and use that to form the rectangle
+    if(widthRatio > heightRatio) {
+        newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio);
+    } else {
+        newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio);
+    }
+    
+    // This is the rect that we've calculated out and this is what is actually used below
+    CGRect rect = CGRectMake(0, 0, newSize.width, newSize.height);
+    
+    // Actually do the resizing to the rect using the ImageContext stuff
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [image drawInRect:rect];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
 #pragma mark - ==================================
 #pragma mark Controls click events
 #pragma mark ==================================
@@ -75,6 +135,35 @@
 #pragma mark Web-service call
 #pragma mark ==================================
 
+- (void)fetchDataFromJSONFile {
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        //-- To show the network indicator until the process is running.
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    });
+    
+    //-- Search Query preparation
+    NSCharacterSet *expectedCharSet = [NSCharacterSet URLQueryAllowedCharacterSet];
+    NSString *urlString = [JSON_FILE_URL stringByAddingPercentEncodingWithAllowedCharacters:expectedCharSet];
+    
+    //-- Preparing a url from predefined link string.
+    NSURL *url = [[NSURL alloc] initWithString:urlString];
+    
+    //-- A url request to fetch data in asynchronous manner.
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
+ 
+    //-- To reset the web-service related values
+    [self resetProperties];
+    
+    //-- To initiate the connection object.
+    _connection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self startImmediately:NO];
+    //-- Schedule the run loop for connection
+    [_connection scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    //-- Start after initiation
+    [_connection start];
+}
+
+/*
 - (void)fetchDataFromJSONFile {
     
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -144,6 +233,81 @@
         }
     }];
 }
+*/
+
+#pragma mark - ==================================
+#pragma mark NSURLConnection delegate functions
+#pragma mark ==================================
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    self.title = @"";
+    [appDelegate displayAnAlertWith:@"Alert !!" andMessage:[error localizedDescription]];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        //-- To hide the network indicator once the response is availble.
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        [_refreshControler endRefreshing];
+    });
+    //-- To reset the web-service related values
+    [self resetProperties];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    if (!_data) {
+        _data = [[NSMutableData alloc] init];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [_data appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    if (_data) {
+        //-- As becuase downaloded data contains special characters first of all we have to conver it into String format.
+        NSString *latinString = [[NSString alloc] initWithData:_data encoding:NSISOLatin1StringEncoding];
+        
+        //-- Now create a data from String content with the help of UTF8Encoding
+        NSData *jsonData = [latinString dataUsingEncoding:NSUTF8StringEncoding];
+        
+        if (latinString != nil && jsonData != nil) {
+            NSError *error = nil;
+            //-- Fetch key-value pair object from a JSON data
+            NSDictionary *dictInfo = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableLeaves error:&error];
+            
+            //-- Display an error if you get at the time of converting a JSON data to an object.
+            if (error != nil) {
+                self.title = @"";
+                [appDelegate displayAnAlertWith:@"Alert !!" andMessage:[NSString stringWithFormat:@"JSON Parsing Error due to : %@", [error localizedDescription]]];
+            } else {
+                NSLog(@"%@", [dictInfo description]);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.title = dictInfo[@"title"];
+                    _arrFacts = [NSArray arrayWithArray:dictInfo[@"rows"]];
+                    if (_arrFacts != nil) {
+                        self.tableView.hidden = NO;
+                        [self.tableView reloadData];
+                    } else {
+                        self.tableView.hidden = YES;
+                    }
+                });
+            }
+        } else {
+            self.title = @"";
+            [appDelegate displayAnAlertWith:@"Alert !!" andMessage:@"An error while encoding data or string conents."];
+        }
+        _data = nil;
+    } else {
+        self.title = @"";
+        [appDelegate displayAnAlertWith:@"Alert !!" andMessage:@"No data available to download or an error while downloading a data."];
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        //-- To hide the network indicator once the response is availble.
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        [_refreshControler endRefreshing];
+    });
+    //-- To reset the web-service related values
+    [self resetProperties];
+}
 
 #pragma mark - ==================================
 #pragma mark Table view data source
@@ -210,39 +374,6 @@
     } @finally {
         return cell;
     }
-}
-
-#pragma mark - ==================================
-#pragma mark User-defined methods
-#pragma mark ==================================
-
-- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    //UIGraphicsBeginImageContext(newSize);
-    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
-    // Pass 1.0 to force exact pixel size.
-    
-    CGSize size = image.size;
-    
-    CGFloat widthRatio  = newSize.width  / image.size.width;
-    CGFloat heightRatio = newSize.height / image.size.height;
-    
-    // Figure out what our orientation is, and use that to form the rectangle
-    if(widthRatio > heightRatio) {
-        newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio);
-    } else {
-        newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio);
-    }
-    
-    // This is the rect that we've calculated out and this is what is actually used below
-    CGRect rect = CGRectMake(0, 0, newSize.width, newSize.height);
-    
-    // Actually do the resizing to the rect using the ImageContext stuff
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
-    [image drawInRect:rect];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return newImage;
 }
 
 /*
