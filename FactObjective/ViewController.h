a68ee4918c79d83133caab4b4806b07764424aba
Nilesh Prajapati <nilesh.prajapati@gatewaytechnolabs.com>
2017-11-21 15:01:39 +0530

    Initial Commit

---
+++ FactObjective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FactObjective
+//
+//  Created by Nilesh Prajapati on 21/11/17.
+//  Copyright © 2017 Nilesh Prajapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FactObjective/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FactObjective
+//
+//  Created by Nilesh Prajapati on 21/11/17.
+//  Copyright © 2017 Nilesh Prajapati. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
